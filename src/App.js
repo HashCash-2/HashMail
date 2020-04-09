65bdf962bc6f35bd3bd83bd53b95e9bb3901c3ea
kautukkundan <kautukkundan@gmail.com>
2020-04-09 23:00:15 +0530

    added left-bar

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 import "./App.scss";
 import Navbar from "./components/navbar";
+import Home from "./components/home";
 
 const history = require("history").createBrowserHistory;
 
@@ -12,6 +13,7 @@ function App() {
       <Router history={history}>
         <Navbar />
       </Router>
+      <Home />
     </div>
   );
 }
