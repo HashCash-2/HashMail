690b9d37767b3a7f625454cfcbb70a7fe0d14e4b
kautukkundan <kautukkundan@gmail.com>
2020-04-17 22:27:50 +0530

    added no emails prompt

--- src/components/Home/mailList.jsx
+++ src/components/Home/mailList.jsx
@@ -34,8 +34,12 @@ const MailList = props => {
     <div className="updates">
       {loading ? (
         <Loader active inline />
+      ) : (whichBox === "inbox" ? inbox : outbox).length === 0 ? (
+        <span className="fadeInUp" style={{ animationDelay: "0.7s" }}>
+          No Emails here
+        </span>
       ) : (
-        (whichBox === "inbox" ? inbox : outbox).map((mail, index) => {
+        null(whichBox === "inbox" ? inbox : outbox).map((mail, index) => {
           return (
             <EachMail
               key={index}
