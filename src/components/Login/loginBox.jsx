7635e793d3e7337cdf089ab6a0b3bbbc2e161af2
kautukkundan <kautukkundan@gmail.com>
2020-04-19 15:05:42 +0530

    refactored login/signup flow and added autologin

--- src/components/Login/loginBox.jsx
+++ src/components/Login/loginBox.jsx
@@ -1,13 +1,10 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
-import Axios from "axios";
 import swal from "sweetalert";
-
-import { URL } from "../../globalvariables";
-import setAuthToken from "../../utils/setAuthToken";
 
 import { Form, Button } from "semantic-ui-react";
 import CustomHeader from "../Common/customHeader";
+import { loginUser } from "../../services/authServices";
 
 const LoginBox = () => {
   const [email, setEmail] = useState("");
@@ -31,28 +28,19 @@ const LoginBox = () => {
       );
     } else {
       setLoading(true);
-      let obj = {};
-      obj.email = email;
-      obj.password = password;
+
       await window.ethereum.enable();
 
-      console.log("login", obj);
-      Axios.post(`${URL}/user/login`, obj)
-        .then(data => {
-          console.log(data);
-          if (data.data.success === true) {
-            setLoading(false);
-            const { token } = data.data;
-            localStorage.setItem("HCtoken", token);
-            setAuthToken(token);
-            history.push("/inbox");
-          }
-        })
-        .catch(error => {
-          console.log(error.response.data);
+      try {
+        const res = await loginUser(email, password);
+        if (res.data.success === true) {
           setLoading(false);
-          swal("Error", "Email or password incorrect", "error");
-        });
+          history.push("/inbox");
+        }
+      } catch (error) {
+        setLoading(false);
+        swal("Error", "Email or password incorrect", "error");
+      }
     }
   };
 
@@ -65,7 +53,7 @@ const LoginBox = () => {
           <input
             type="email"
             onChange={e => setEmail(e.target.value)}
-            placeholder="eg : joe@gmail.com"
+            placeholder="eg : jenna@gmail.com"
             name="username"
           />
         </Form.Field>
---
+++ src/services/apiUrl.js
@@ -0,0 +1,4 @@
+export default {
+  USER_LOGIN: "/user/login",
+  USER_SIGNUP: "/user/register"
+};
